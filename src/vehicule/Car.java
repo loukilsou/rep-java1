ad1fe9a4fe30c2c72a118135e2748757844403ea
s.loukil <loukilsou@gmx.fr.com>
2019-11-25 15:46:29 +0100

    mise en place du repositoire distant : exemple

---
+++ src/vehicule/Car.java
@@ -0,0 +1,14 @@
+package vehicule;
+
+public class Car {	
+
+//public void fulThrottle() {
+//System.out.println("the car is going as fastas it can!");}	
+
+//public void speed(int maxSpeed) {
+//	System.out.println("Max speed is:"+maxSpeed );}
+//}
+
+	
+	
+}
---
+++ src/vehicule/Cartest.java
@@ -0,0 +1,12 @@
+package vehicule;
+
+public class Cartest {
+
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Car myCar=new Car();
+//myCar.fulThrottle();
+//myCar.speed(200);
+//}
+//}
+}
